137400943656833d4e892352bc9795f6925b9f33
Adrian <qwizi95@protonmail.com>
2020-08-29 23:38:01 +0200

    Updated QueryResource

--- shoppy/__init__.py
+++ shoppy/__init__.py
@@ -2,6 +2,31 @@ import requests
 import simplejson as json
 
 
+class ListMixin:
+    def list(self):
+        return self.session.get(f"{self.api_url}{self.endpoint}")
+
+
+class CreateMixin:
+    def create(self, data):
+        return self.session.put(f"{self.api_url}{self.endpoint}", data=json.dumps(data))
+
+
+class RetrieveMixin:
+    def retrieve(self, obj_id):
+        return self.session.get(f"{self.api_url}{self.endpoint}{obj_id}/")
+
+
+class UpdateMixin:
+    def update(self, obj_id, updated_data):
+        return self.session.post(f"{self.api_url}{self.endpoint}{obj_id}/", data=json.dumps(updated_data))
+
+
+class DeleteMixin:
+    def delete(self, obj_id):
+        return self.session.delete(f"{self.api_url}{self.endpoint}{obj_id}/")
+
+
 class ResourceClient:
     endpoint = None
     api_url = "https://shoppy.gg/api"
@@ -10,49 +35,27 @@ class ResourceClient:
         self.session = session
 
 
-
-class List:
-    def list(self):
-        return self.session.get(f"{self.api_url}{self.endpoint}")
-
-
-class Create:
-    def create(self, data):
-        return self.session.put(f"{self.api_url}{self.endpoint}", data=json.dumps(data))
-
-
-class Retrieve:
-    def retrieve(self, obj_id):
-        return self.session.get(f"{self.api_url}{self.endpoint}{obj_id}/")
-
-
-class Update:
-    def update(self, obj_id, updated_data):
-        return self.session.post(f"{self.api_url}{self.endpoint}{obj_id}/", data=json.dumps(updated_data))
-
-
-class Delete:
-    def delete(self, obj_id):
-        return self.session.delete(f"{self.api_url}{self.endpoint}{obj_id}/")
-
-
-class Product(ResourceClient, List, Create, Retrieve, Update, Delete):
+class ProductResource(ResourceClient, ListMixin, CreateMixin, RetrieveMixin, UpdateMixin, DeleteMixin):
     endpoint = "/v1/products/"
 
 
-class Order(ResourceClient, List, Retrieve):
+class OrderResource(ResourceClient, ListMixin, RetrieveMixin):
     endpoint = "/v1/orders/"
 
 
-class FeedBack(ResourceClient, List, Retrieve):
+class FeedBackResource(ResourceClient, ListMixin, RetrieveMixin):
     endpoint = "/v1/feedbacks/"
 
 
-class Query(ResourceClient, List, Retrieve, Update):
+class QueryResource(ResourceClient, ListMixin, RetrieveMixin):
     endpoint = "/v1/queries/"
+    STATUS = ('close', 'reopen')
 
     def reply(self, query_id, message):
         return self.session.post(f"{self.endpoint}{query_id}/", data=json.dumps({'message': message}))
+
+    def update(self, obj_id, updated_data, action):
+        return self.session.post(f"{self.api_url}{self.endpoint}{obj_id}/{action}", data=json.dumps(updated_data))
 
 
 class Shoppy:
@@ -64,7 +67,7 @@ class Shoppy:
             'Content-Type': 'application/json',
             'User-Agent': 'Qwizi App'
         })
-        self.product = Product(self.session)
-        self.order = Order(self.session)
-        self.feedback = FeedBack(self.session)
-        self.query = Query(self.session)
+        self.product = ProductResource(self.session)
+        self.order = OrderResource(self.session)
+        self.feedback = FeedBackResource(self.session)
+        self.query = QueryResource(self.session)
